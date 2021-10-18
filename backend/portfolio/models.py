9a9c7d7f5d371abbb723d964fd3639d7f4f1ffff
Fernand Jose Diaz Perez <fernand@oti-27.vit.gob.ve>
2021-10-18 17:03:48 -0400

    ADD: Migraciones de los proyectos  -Agregado el modelo de proyecto con sus columnas y creado la migracion en sqlite

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+	name = models.CharField(max_length=200)
+	description = models.TextField()
+	project_link = models.CharField(max_length=200)
+	image_folder = models.CharField(max_length=200)
+	created_at = models.DateTimeField(auto_now_add=True)
+		
